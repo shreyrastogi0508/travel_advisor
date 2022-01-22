f939bbdb4b302dc8bf27ed2769c7b1917595d290
Shrey Rastogi <73610824+shreyrastogi0508@users.noreply.github.com>
2022-01-22 22:00:14 +0530

    Update Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,33 +1,10 @@
-import React from "react";
-import googleMapReact from "google-map-react";
-import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import { LocationSearchingOnOutlined } from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab';
-
-import useStyles from './styles';
+import React from 'react';
 
 const Map = () => {
-    const classes = useStyles();
-    const isMobile = useMediaQuery('(min-width:600px');
-    const coordinates = { lat:0, long:0 };
-
-    
-    
     return (
-        <div className={classes.mapContainer}>
-            <googleMapReact 
-                bootstrapURLKeys = {{ key: ''}}
-                defaultCenter={coordinates}
-                center={coordinates}
-                defaultZoom={14}
-                margin={[50,50,50,50]}
-                options={''}
-                onChange={''}
-
-                onChildClick={''}
-            ></googleMapReact>
-        </div>
+    <h1>Map</h1>
     );
+
 }
 
 export default Map;
