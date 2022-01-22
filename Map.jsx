20757ec6319e7cf32430cafef633d5dbeec07cab
Naveen Parthasarathy <77956540+naveen-parth@users.noreply.github.com>
2022-01-22 21:54:34 +0530

    Add files via upload

---
+++ Map.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import googleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import { LocationSearchingOnOutlined } from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
+
+const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px');
+    const coordinates = { lat:0, long:0 };
+
+    
+    
+    return (
+        <div className={classes.mapContainer}>
+            <googleMapReact 
+                bootstrapURLKeys = {{ key: ''}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+
+                onChildClick={''}
+            ></googleMapReact>
+        </div>
+    );
+}
+
+export default Map;
